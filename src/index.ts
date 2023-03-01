8e92b5d6a6c22d389fd4968524b75f54cb9a6f82
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-03-01 21:51:44 +0200

    Add CSS & fonts to "How to use this" page

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,9 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
+
 app.use(router);
 app.use(errorHandler);
 
