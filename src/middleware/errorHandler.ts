edf0b526c8eff0f5919891b64b6c046683521d06
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-03-01 21:56:44 +0200

    Ignore unused `next` in errorHandler

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,5 +1,6 @@
 import type { ErrorRequestHandler } from 'express';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send(`<pre>${err.stack}</pre>`);
