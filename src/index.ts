88a173b76725e5c8e49e5deaf96099994ed844aa
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-03-01 20:28:08 +0200

    Add `ejs`

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+import path from 'path';
 import express from 'express';
 import cors from 'cors';
 
@@ -9,6 +10,9 @@ import router from 'routes';
 const PORT = process.env.PORT || 8000;
 
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.json());
 app.use(cors());
